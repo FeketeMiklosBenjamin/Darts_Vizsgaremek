007db155c7f108d25e22180c2d118007252a2d1d
DomiNate2 <jdomi04@gmail.com>
2025-02-05 13:20:10 +0100

    Bejelentkezés

--- Darts_Club_MainProject/src/components/sign_in_comp/Sign_Form.vue
+++ Darts_Club_MainProject/src/components/sign_in_comp/Sign_Form.vue
@@ -4,21 +4,16 @@
 
 <template>
     <div class="position-rel">
-        <div class="container z-1 transform align-items-center glass-card width p-2 px-3">
+        <div class="container z-1 transform align-items-center glass-card width p-4">
             <div class="row">
-                <div class="col-12  mb-2">
-                    <h1 class="text-center display-4 text-light">Regisztráció</h1>
+                <div class="col-12 mb-2">
+                    <h1 class="text-start display-4 text-light">Bejelentkezés</h1>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-12 col-md-12 mx-auto">
                     <form>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="name" placeholder="Név">
-                            <label for="name">Név</label>
-                        </div>
-
                         <div class="form-floating mb-3">
                             <input type="email" class="form-control" id="email" placeholder="E-mail">
                             <label for="email">E-mail</label>
@@ -28,14 +23,8 @@
                             <input type="password" class="form-control" id="password" placeholder="Jelszó">
                             <label for="password">Jelszó</label>
                         </div>
-
-                        <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="confirm_password" placeholder="Jelszó újra">
-                            <label for="confirm_password">Jelszó újra</label>
-                        </div>
-
                         <div class="mb-3">
-                            <button type="submit" class="btn btn-info w-100 py-2">Regisztráció</button>
+                            <button type="submit" class="btn btn-warning w-100 py-2">Bejelentkezés</button>
                         </div>
 
                     </form>
