20962523e06d46fca2939e34a5b3d35119e67b93
norbert-gemes <gemes.norbert@students.jedlik.eu>
2025-03-03 23:06:08 +0100

    Versenyek oldal dízájnja majdnem kész, illetve a statisztikai adatok lekérése és megjelenítése a Statisztika Oldalon sikeres

--- DartsMobilApp/DartsMobilApp/Classes/StatisticModel.cs
+++ DartsMobilApp/DartsMobilApp/Classes/StatisticModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace DartsMobilApp.Classes
 {
+
     public class StatisticModel
     {
+        public string id { get; set; }
+        public string userId { get; set; }
+        public int matches { get; set; }
+        public int matchesWon { get; set; }
+        public int sets { get; set; }
+        public int setsWon { get; set; }
+        public int legs { get; set; }
+        public int legsWon { get; set; }
+        public float averages { get; set; }
+        public int max180s { get; set; }
+        public float checkoutPercentage { get; set; }
+        public int highestCheckout { get; set; }
+        public int nineDarter { get; set; }
     }
+
 }
--- DartsMobilApp/DartsMobilApp/ViewModel/StatisticViewModel.cs
+++ DartsMobilApp/DartsMobilApp/ViewModel/StatisticViewModel.cs
@@ -7,33 +7,55 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using DartsMobilApp.Classes;
+using System.Text.Json;
 
 namespace DartsMobilApp.ViewModel
 {
     public partial class StatisticViewModel : ObservableObject
     {
 
+
         string? AToken = SecureStorage.GetAsync("Token")?.Result?.ToString();
         HttpClient httpClient = new();
 
+        [ObservableProperty]
+
+        public StatisticModel playerStatistic;
 
         [RelayCommand]
 
         private void Appearing()
         {
-
+            GetStatistics();
         }
 
 
-        private async StatisticModel GetStatistics()
+        private async Task<StatisticModel> GetStatistics()
         {
-            using (var requestMessage =
-            new HttpRequestMessage(HttpMethod.Get, "https://disciplinary-marj-feketemiklos222-91053eff.koyeb.app/api/users/friendlystat"))
             {
-                requestMessage.Headers.Authorization =
-                    new AuthenticationHeaderValue("Bearer", AToken);
+                if (string.IsNullOrEmpty(AToken))
+                {
+                    throw new Exception("Access token is missing. Please log in.");
+                }
 
-                await httpClient.SendAsync(requestMessage);
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AToken);
+
+                var url = "https://disciplinary-marj-feketemiklos222-91053eff.koyeb.app/api/users/friendlystat"; 
+
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    PlayerStatistic = JsonSerializer.Deserialize<StatisticModel>(json);
+
+                    return PlayerStatistic;
+                }
+                else
+                {
+                    throw new Exception($"Error fetching data: {response.StatusCode}");
+                }
             }
         }
     }
