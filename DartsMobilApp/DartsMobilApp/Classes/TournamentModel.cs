35185e30486781c9389373dcef9b2bc42ba9eb6e
norbert-gemes <gemes.norbert@students.jedlik.eu>
2025-03-03 11:34:18 +0100

    Versenyek oldal dizájn halad

---
+++ DartsMobilApp/DartsMobilApp/Classes/TournamentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsMobilApp.Classes
+{
+    public class TournamentModel
+    {
+        public string Name { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
--- DartsMobilApp/DartsMobilApp/ViewModel/CompetitionsViewModel.cs
+++ DartsMobilApp/DartsMobilApp/ViewModel/CompetitionsViewModel.cs
@@ -1,13 +1,24 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Android.Database;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DartsMobilApp.Classes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DartsMobilApp.ViewModel
 {
+
     public partial class CompetitionsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public ObservableCollection<TournamentModel> tournaments = new ObservableCollection<TournamentModel>
+        {
+            new TournamentModel { Name = "Hungarian Darts Trophy", Level = "Profi", Time = DateTime.Now },
+            new TournamentModel { Name = "UK Open", Level = "Amatőr", Time = DateTime.Now }
+        };
+
     }
 }
