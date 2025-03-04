74995bea02e5428e99a52dbe1e6fc7128fc9b6fb
norbert-gemes <gemes.norbert@students.jedlik.eu>
2025-03-04 11:36:28 +0100

    Friendly Stat oldal dízájn alakulóban

--- DartsMobilApp/DartsMobilApp/ViewModel/FriendlySetupViewModel.cs
+++ DartsMobilApp/DartsMobilApp/ViewModel/FriendlySetupViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,19 @@ namespace DartsMobilApp.ViewModel
 {
     public partial class FriendlySetupViewModel : ObservableObject
     {
+        [RelayCommand]
+
+        private void RecolorButton(Button BTN)
+        {
+            if (BTN.BackgroundColor == Color.FromRgb(211,211,211))
+            {
+                BTN.BackgroundColor = Color.FromRgb(255, 165, 0);
+            }
+            else
+            {
+                BTN.BackgroundColor = Color.FromRgb(211, 211, 211);
+            }
+        }
+        
     }
 }
