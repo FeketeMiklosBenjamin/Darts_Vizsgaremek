fbf0a428aa952afc34db2a8ce7e0adb4cf37f53a
DomiNate2 <jdomi04@gmail.com>
2025-03-18 09:38:21 +0100

    Statistic page-box tervezés

--- Darts_Club_MainProject/src/components/statistic_comp/Statistic.vue
+++ Darts_Club_MainProject/src/components/statistic_comp/Statistic.vue
@@ -9,7 +9,7 @@ const { status, user } = storeToRefs(useUserStore());
 <template>
     <div class="background_color-feed">
         <div class="row">
-            <div class="col-5 mt-5">
+            <div class="col-4 mt-5">
                 <div class="bg-secondary rounded-circle border border-5 statistic-img" :class="{
                     'border-success': user.level == 'Amateur',
                     'border-warning': user.level == 'Advanced',
@@ -18,14 +18,16 @@ const { status, user } = storeToRefs(useUserStore());
                     <img :src=user.profilePictureUrl class="statistic-profileImg d-block" alt="Nincs">
                 </div>
                 <h1 class="display-6 text-white margin-statname">{{ user.username }}</h1>
+                <div class="col-5">
+                <button class="btn btn-warning px-1 marginst-btn">Módosítás</button>
+            </div>
             </div>
             <div class="col-7">
-                <p class="text-white">Szia</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-5">
-                <button class="btn btn-warning w-25 px-1 marginst-btn">Módosítás</button>
+                <div class="statistic-box">
+                    <div class="statistic-line-vertical statistic-tline-margin-hor"></div>
+                    <div class="statistic-line-horizontal statistic-line-margin-ver"></div>
+                    <div class="statistic-line-vertical statistic-bline-margin-hor"></div>
+                </div>
             </div>
         </div>
     </div>
